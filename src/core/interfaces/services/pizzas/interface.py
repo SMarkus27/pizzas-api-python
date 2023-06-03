80f968dc4d7c34a9506e20c7140ea1a27a696bc1
SMarkus27 <pereira.markus.27@hotmail.com>
2023-06-03 14:16:41 -0300

    chore: created pizza service interface

---
+++ src/core/interfaces/services/pizzas/interface.py
@@ -0,0 +1,35 @@
+from abc import ABCMeta, abstractmethod
+
+from src.repositories.pizzas.repository import PizzasRepository
+
+
+class IPizzaService(metaclass=ABCMeta):
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return True
+
+    @classmethod
+    @abstractmethod
+    async def create_pizza(cls, data: dict, pizza_repo=PizzasRepository):
+        pass
+
+    @classmethod
+    @abstractmethod
+    async def find_all_pizzas(cls, pizza_repo=PizzasRepository):
+        pass
+
+    @classmethod
+    @abstractmethod
+    async def find_one_pizza(cls, pizza_repo=PizzasRepository):
+        pass
+
+    @classmethod
+    @abstractmethod
+    async def update_pizza(cls, pizza_repo=PizzasRepository):
+        pass
+
+    @classmethod
+    @abstractmethod
+    async def delete_pizza(cls, pizza_repo=PizzasRepository):
+        pass
